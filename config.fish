6875e5d701d8ee95bcf2b0060ceb9f97f8fb9291
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2021-11-27 18:10:55 -0500

    My .fish
    very nice code

---
+++ config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+# (lol linus) package managers alias
+
+# winget aliases
+alias windate="paru -Sy"
+alias wingrade="paru -Syu"
+alias winget="paru"
+#yay aliases
+alias mget="yay"
+# extra app aliases
+alias cmd="bash"
+alias pwsh="fish"
+alias notepad="kate"
+# neofetch aliases
+alias winfetch="neofetch"
+alias winfo="neofetch"
+alias winver="lsb_release -a"
+#
+
+    # Commands to run in interactive sessions can go here
+end
