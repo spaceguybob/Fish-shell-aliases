4dc690383cffccad5927858a727dd5e506b4708f
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2021-12-19 19:33:21 -0500

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,21 +1,46 @@
+
 if status is-interactive
+
+
 # (lol linus) package managers alias
 
-# winget aliases
-alias windate="paru -Sy"
-alias wingrade="paru -Syu"
-alias winget="paru"
-#yay aliases
-alias mget="yay"
-# extra app aliases
-alias cmd="bash"
-alias pwsh="fish"
-alias notepad="kate"
-# neofetch aliases
-alias winfetch="neofetch"
-alias winfo="neofetch"
-alias winver="lsb_release -a"
-#
 
-    # Commands to run in interactive sessions can go here
+
+# winget aliases
+
+alias windate="paru -Sy"
+
+alias wingrade="paru -Syu"
+
+alias winget="paru"
+
+#yay aliases
+
+alias mget="yay"
+
+# extra app aliases
+
+alias cmd="bash"
+
+alias pwsh="fish"
+
+alias notepad="kate"
+
+# neofetch aliases
+
+alias winfetch="neofetch"
+
+alias winfo="neofetch"
+
+alias winver="lsb_release -a"
+
+# extra other
+
+alias sudo=doas
+alias cl=clear
+alias l=ls
+
+# Commands to run in interactive sessions can go here
+
 end
+
