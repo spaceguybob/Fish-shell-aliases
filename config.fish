7817d5d668f36541029d5fa3302f8230683ffe18
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2022-06-10 03:39:34 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,32 +1,4 @@
-
-
 if status is-interactive
-
-#assaign your package manager here
-alias packager="apt"
-#update aliases
-
-alias upd="sudo pmm update"
-
-alias upg="sudo pmm upgrade"
-
-alias rup="sudo pmm update && sudo pmm upgrade && sudo reboot"
-
-alias sup="sudo pmm update && sudo pmm upgrade && sudo poweroff"
-
-# (lol linus) package managers alias
-alias emerge="sudo apt install"
-# winget aliases
-
-alias windate="sudo xbps-install -Sy"
-
-alias wingrade="sudo xbps-install -Syu"
-
-alias winget="sudo packager"
-
-#yay aliases
-
-alias mget=yay
 
 # extra app aliases
 
@@ -35,8 +7,6 @@ alias cmd=sh
 alias neosh=fish
 
 alias bsh=bash
-
-alias pwsh=fish
 
 alias notepad=gedit
 
@@ -68,6 +38,9 @@ alias ct="clear && uptime -p"
 alias l=ls
 
 alias logo="neofetch -l"
+
+#ETC
+zoxide init fish | source
 
 #only use this if you have startship installed 
 ##starship init fish | source
