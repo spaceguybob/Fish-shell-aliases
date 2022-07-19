5411d9bb67a7fbceee7d1b5b76fd11a4c7c47ecf
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2022-07-19 14:25:50 -0400

    Update config.fish

--- config.fish
+++ config.fish
@@ -13,6 +13,7 @@ alias notepad=gedit
 alias rootpad="sudo kate"
 
 
+
 # neofetch aliases
 
 alias winfetch="neofetch"
@@ -44,4 +45,22 @@ zoxide init fish | source
 
 #only use this if you have startship installed 
 ##starship init fish | source
+
+##uutils
+alias cat=" /home/space/.cargo/bin/coreutils cat"
+alias yes=" /home/space/.cargo/bin/coreutils yes"
+#alias uname=" /home/space/.cargo/bin/coreutils uname"
+alias uname2=" /usr/bin/uname"
+alias rm=" /home/space/.cargo/bin/coreutils rm"
+alias mv=" /home/space/.cargo/bin/coreutils mv"
+alias nice=" /home/space/.cargo/bin/coreutils nice"
+alias mkdir=" /home/space/.cargo/bin/coreutils mkdir"
+alias ln=" /home/space/.cargo/bin/coreutils ln"
+alias kill=" /home/space/.cargo/bin/coreutils kill"
+alias echo=" /home/space/.cargo/bin/coreutils echo"
+alias chmod=" /home/space/.cargo/bin/coreutils chmod"
+alias ls=" /home/space/.cargo/bin/coreutils ls"
+alias cp=" /home/space/.cargo/bin/coreutils cp"
+
 end
+thefuck --alias | source
