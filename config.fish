cdb058853914007791f8a11072f0328f94f2c5ec
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2022-08-17 16:48:04 -0400

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
+    # Commands to run in interactive sessions can go here
+alias upd="rpm-ostree update"
 
-# extra app aliases
+alias rebootd="rpm-ostree update && reboot"
+
+alias neofetch=fastfetch
 
 alias cmd=sh
 
@@ -11,8 +15,6 @@ alias bsh=bash
 alias notepad=gedit
 
 alias rootpad="sudo kate"
-
-
 
 # neofetch aliases
 
@@ -40,27 +42,6 @@ alias l=ls
 
 alias logo="neofetch -l"
 
-#ETC
-zoxide init fish | source
-
-#only use this if you have startship installed 
-##starship init fish | source
-
-##uutils
-alias cat=" /home/space/.cargo/bin/coreutils cat"
-alias yes=" /home/space/.cargo/bin/coreutils yes"
-#alias uname=" /home/space/.cargo/bin/coreutils uname"
-alias uname2=" /usr/bin/uname"
-alias rm=" /home/space/.cargo/bin/coreutils rm"
-alias mv=" /home/space/.cargo/bin/coreutils mv"
-alias nice=" /home/space/.cargo/bin/coreutils nice"
-alias mkdir=" /home/space/.cargo/bin/coreutils mkdir"
-alias ln=" /home/space/.cargo/bin/coreutils ln"
-alias kill=" /home/space/.cargo/bin/coreutils kill"
-alias echo=" /home/space/.cargo/bin/coreutils echo"
-alias chmod=" /home/space/.cargo/bin/coreutils chmod"
-alias ls=" /home/space/.cargo/bin/coreutils ls"
-alias cp=" /home/space/.cargo/bin/coreutils cp"
-
 end
+
 thefuck --alias | source
